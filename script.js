51dd48ec3ed1979fb508476a33a629c7c29ebc70
kevinserrano <66225834+kevinserrano@users.noreply.github.com>
2020-08-31 10:46:04 -0500

    adding click to reflect chart

--- script.js
+++ script.js
@@ -1,38 +1,48 @@
 var ctx = $("#myChart");
 
-var complete = 1;
+var complete = 0;
 var incomplete = 0;
 var inProgress = 0;
+var completeButton = $(".complete");
 
-var myChart = new Chart(ctx, {
-    type: "pie",
-    data: {
-        labels: ["Complete", "In Progress", "In Complete"],
-        datasets: [{
-            label: "Tasks Completion Status",
-            data: [complete++, incomplete++, inProgress++],
-            backgroundColor: [
-                "hsl(186, 100%, 50%)",
-                "hsl(60, 100%, 85%)",
-                "hsl(1, 100%, 70%)",
-            ],
-            borderColor: [
-                "hsl(240, 100%, 50%)",
-                "hsl(60, 100%, 50%)",
-                "hsl(0, 100%, 50%)",
-            ],
-            borderWidth: 2
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
+
+
+$(document).ready(function () {
+
+    $(completeButton).on("click", function () {
+
+        var myChart = new Chart(ctx, {
+            type: "pie",
+            data: {
+                labels: ["Complete", "In Progress", "In Complete"],
+                datasets: [{
+                    label: "Tasks Completion Status",
+                    data: [complete++, incomplete++, inProgress++],
+                    backgroundColor: [
+                        "hsl(186, 100%, 50%)",
+                        "hsl(60, 100%, 85%)",
+                        "hsl(1, 100%, 70%)",
+                    ],
+                    borderColor: [
+                        "hsl(240, 100%, 50%)",
+                        "hsl(60, 100%, 50%)",
+                        "hsl(0, 100%, 50%)",
+                    ],
+                    borderWidth: 2
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
                 }
-            }]
-        }
-    }
+            }
+        });
+    });
+
 });
 
 var toDoBtn = document.getElementById("addtodo");
