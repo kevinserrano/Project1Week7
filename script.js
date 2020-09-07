528cd9e3f8dda7723081161f502956a0128028a1
Analee Guerra <aguerra1508@gmail.com>
2020-09-06 20:22:49 -0500

    made buttons into separate functions, got items out of local storage and back to page

--- script.js
+++ script.js
@@ -3,45 +3,9 @@ $(document).ready(function () {
     getProjects();
 
     var ctx = $("#myChart");
-
     var complete = 0;
     var incomplete = 0;
     var inProgress = 0;
-
-
-
-    var myChart = new Chart(ctx, {
-        type: "pie",
-        data: {
-            labels: ["Complete", "In Progress", "Incomplete"],
-            datasets: [{
-                label: "Tasks Completion Status",
-                data: [complete, inProgress, incomplete],
-                backgroundColor: [
-                    "hsl(186, 100%, 50%)",
-                    "hsl(60, 100%, 85%)",
-                    "hsl(1, 100%, 70%)",
-                ],
-                borderColor: [
-                    "hsl(240, 100%, 50%)",
-                    "hsl(60, 100%, 50%)",
-                    "hsl(0, 100%, 50%)",
-                ],
-                borderWidth: 2
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
-    });
-
-
     var projects = [];
     var addresses = [];
 
@@ -50,7 +14,6 @@ $(document).ready(function () {
         if (this.classList.contains("complete")) {
             console.log("complete clicked");
             complete++;
-
             inProgress = 0;
             incomplete = 0;
         } else if (this.classList.contains("inprogress")) {
@@ -58,9 +21,7 @@ $(document).ready(function () {
             complete = 0;
             incomplete = 0;
         } else {
-
             return;
-
         }
 
         var myChart = new Chart(ctx, {
@@ -93,9 +54,8 @@ $(document).ready(function () {
                 }
             }
         });
-
-
     });
+
     var toDoBtn = document.getElementById("addtodo");
     toDoBtn.addEventListener("click", function (event) {
         event.preventDefault();
@@ -107,36 +67,12 @@ $(document).ready(function () {
         addresses.push(address);
         localStorage.setItem("address", addresses);
         console.log(addresses);
-
         var projectName = document.getElementById("project-name").value;
+        console.log(projectName);
         projects.push(projectName);
         localStorage.setItem("project", projects);
         console.log(projects);
-
-        var addToList = document.getElementById("to-do-list");
-        var addListEl = document.createElement("li");
-
-        var formEl = document.createElement("form");
-        var labelEl = document.createElement("label");
-        var spanEl = document.createElement("span");
-
-        var compEl = document.createElement("input");
-        compEl.setAttribute("class", "complete");
-        compEl.setAttribute("type", "radio");
-        compEl.setAttribute("name", "group1")
-        var incompEl = document.createElement("input");
-        compEl.setAttribute("class", "inprogress");
-        incompEl.setAttribute("type", "radio");
-        incompEl.setAttribute("name", "group1")
-        addToList.append(addListEl);
-        addListEl.append(formEl);
-        formEl.append(labelEl);
-        labelEl.append(compEl);
-        labelEl.append(spanEl);
-        spanEl.innerHTML = "Complete";
-        labelEl.append(incompEl);
-        labelEl.append(spanEl);
-        spanEl.innerHTML = "Inprogress";
+        renderBtns();
         /*var settings = {
             "async": true,
             "crossDomain": true,
@@ -146,7 +82,6 @@ $(document).ready(function () {
                 "x-rapidapi-host": "google-maps-geocoding.p.rapidapi.com",
                 "x-rapidapi-key": "bac4682d6fmsh029578abb1cefd5p1e11bbjsn558f94329cba",
             }
-
         }
         $.ajax(settings).done(function (response) {
             console.log(response);
@@ -175,44 +110,41 @@ $(document).ready(function () {
 
     function getProjects() {
         if (localStorage.getItem("project") === null && localStorage.getItem("address") === null) {
-
             console.log("No stored items")
         } else {
             var storedProjects = localStorage.getItem("project");
             var storedAddresses = localStorage.getItem("address");
             var commaReplace = storedAddresses.replace(/%252C/g, ",");
             var fixedAddress = commaReplace.replace(/%20/g, " ");
-
-            var addToList = document.getElementById("to-do-list");
-            var addListEl = document.createElement("li");
-
-            var formEl = document.createElement("form");
-            var labelEl = document.createElement("label");
-            var spanEl = document.createElement("span");
-
-            var compEl = document.createElement("input");
-            compEl.setAttribute("class", "complete");
-            compEl.setAttribute("type", "radio");
-            compEl.setAttribute("name", "group1")
-
-            var incompEl = document.createElement("input");
-            incompEl.setAttribute("type", "radio");
-            incompEl.setAttribute("name", "group1")
-
-            addToList.append(addListEl);
-            addListEl.append(formEl);
-            formEl.append(labelEl);
-            labelEl.append(compEl);
-            labelEl.append(spanEl);
-
-            spanEl.innerHTML = "Complete";
-
-            addToList.append(addListEl);
-
-            addListEl.append(storedProjects);
-
-
+            console.log(storedProjects);
+            var splitProjects = storedProjects.split(",");
+            console.log(splitProjects);
+            for (var i = 0; i < splitProjects.length; i++){
+                renderBtns();
+            }
         }
     }
 
+    function renderBtns() {
+        var addToList = document.getElementById("to-do-list");
+        var addListEl = document.createElement("li");
+        var formEl = document.createElement("form");
+        var labelEl = document.createElement("label");
+        var spanEl = document.createElement("span");
+        var compEl = document.createElement("input");
+        compEl.setAttribute("class", "complete");
+        compEl.setAttribute("type", "radio");
+        compEl.setAttribute("name", "group1")
+        var incompEl = document.createElement("input");
+        incompEl.setAttribute("class", "inprogress");
+        incompEl.setAttribute("type", "radio");
+        incompEl.setAttribute("name", "group1")
+        addToList.append(addListEl);
+        //addListEl.append(projectName);
+        addListEl.append(formEl);
+        formEl.append(labelEl);
+        labelEl.append(compEl);
+        labelEl.append(spanEl);
+        spanEl.innerHTML = "Complete";
+    }
 });
